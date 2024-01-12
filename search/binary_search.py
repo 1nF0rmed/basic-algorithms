6f7dc6003b576a080b6a0ca218fee4b809bb215c
1nF0rmed <prkinformed@gmail.com>
2024-01-12 14:12:39 +0530

    implement binary search

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,5 +1,18 @@
 from search.search import Search
 
 class BinarySearch(Search):
-    pass
+    def search(self, list, item):
+        left = 0
+        right = len(list) - 1
+        while left<=right:
+            mid = int((left+right)/2)
+
+            if list[mid] == item:
+                return mid
+            elif list[mid] < item:
+                left = mid
+            else:
+                right = mid
+        
+        return None
 
