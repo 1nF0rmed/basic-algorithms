ac167831b0b9312aab45da3bd97c01ef9a19e291
1nF0rmed <prkinformed@gmail.com>
2024-01-13 22:30:12 +0530

    attempting to add time check tests

--- search/test/test_binary_search.py
+++ search/test/test_binary_search.py
@@ -18,3 +18,14 @@ def test_binary_long_array_search(binary_search):
     item = list[item_index]
     result = binary_search.search(list, item)
     assert result == item_index
+
+def test_binary_array_search_time(binary_search):
+    list = [i for i in range(1, 100000001)]
+    item_index = random.randint(0, 99999999)
+    item = list[item_index]
+    import time
+    start_time = time.time()
+    result = binary_search.search(list, item)
+    end_time = time.time()
+    assert result == item_index
+    assert end_time - start_time < 0.5
